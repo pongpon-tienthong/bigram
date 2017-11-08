cecd2c560b595ab2f8b2c6b3e0be4b956800f501
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2017-11-08 14:10:34 -0600

    Init Project

---
+++ src/main/java/pongpon/springframework/BigramApplication.java
@@ -0,0 +1,12 @@
+package pongpon.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigramApplication.class, args);
+	}
+}
